c61f40ae2e1da5d49c72409de01e1719f99bed1d
scarabey35 <den.zzz168@gmail.com>
2025-05-02 12:30:24 +0300

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,6 +1,9 @@
 from app import create_app
+from flask_socketio import SocketIO
+import eventlet
 
 app = create_app()
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
