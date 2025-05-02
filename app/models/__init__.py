8668a950ddd1c830f18baed576a8685e322bb27a
scarabey35 <den.zzz168@gmail.com>
2025-05-02 12:21:09 +0300

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,7 @@
+from app.models.user import User
+from app.models.event import Event
+from app.models.task import Task
+from app.models.event_invite import EventInvite
+from app.models.event_participant import EventParticipant
+from app.models.event_comment import EventComment
 
