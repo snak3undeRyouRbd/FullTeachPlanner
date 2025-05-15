75393bc727028b1f3778798c47c00f74863b1320
scarabey35 <den.zzz168@gmail.com>
2025-05-15 18:32:12 +0300

    Delete backend directory

--- backend/index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const app = require('./app'); // Импортируем app из app.js
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Сервер запущен на http://localhost:${PORT}`));
