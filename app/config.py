154f08c06b76fbba4df1d3472402fe178bccf969
scarabey35 <den.zzz168@gmail.com>
2025-05-15 18:26:12 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///site.db')
+    SECRET_KEY = 'your_secret_key_here'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
